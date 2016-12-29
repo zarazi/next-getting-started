1327aceca9fe3153da7e04ece7f2c8d9b7c1f86a
zarazi <zarazi@hotmail.com>
2016-12-29 20:40:19 +0700

    Use pathname from props

--- components/header-menu.js
+++ components/header-menu.js
@@ -4,20 +4,17 @@ import Router, { route } from 'next/router'
 import { Input, Menu, Container } from 'semantic-ui-react'
 
 export default class HeaderMenu extends Component {
-  state = { currentRoute: this.props.pathname }
-
   handleItemClick = (e, { name }) => Router.push(name)
 
   render() {
-    const { currentRoute } = this.state
+    const route = this.props.pathname
     return (
       <div>
-        <h3>{this.props.pathname}</h3>
         <Menu secondary inverted fixed="top" color="teal">
           <Container fluid>
             <Menu.Item header><Link href="/"><a>KK-Pedia</a></Link></Menu.Item>
-            <Menu.Item header name='/' content="Home" active={currentRoute === '/'} onClick={this.handleItemClick} />
-            <Menu.Item name='/story' content="Story" active={currentRoute === '/story'} onClick={this.handleItemClick} />
+            <Menu.Item header name='/' content="Home" active={route === '/'} onClick={this.handleItemClick} />
+            <Menu.Item name='/story' content="Story" active={route === '/story'} onClick={this.handleItemClick} />
             <Menu.Menu position='right'>
               <Menu.Item>
                 <Input icon='search' placeholder='Search...' />

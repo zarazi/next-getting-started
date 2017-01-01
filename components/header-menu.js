1eaa0f67e27ec6c910b300fac0af8b322355a510
zarazi <zarazi@hotmail.com>
2017-01-02 00:57:30 +0700

    Show loading on route changing

--- components/header-menu.js
+++ components/header-menu.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
+import Head from 'next/head'
 import Link, { prefetch } from 'next/prefetch'
 import Router, { route } from 'next/router'
 import { Input, Menu, Container } from 'semantic-ui-react'
+import NProgress from 'nprogress'
+
+Router.onRouteChangeStart = (url) => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+}
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
 
 export default class HeaderMenu extends Component {
   handleItemClick = (e, { name }) => setTimeout(() => Router.push(name), 100)
@@ -9,6 +18,11 @@ export default class HeaderMenu extends Component {
     const route = this.props.pathname
     return (
       <div>
+        <Head>
+          {/* Import CSS for nprogress */}
+          <link rel='stylesheet' type='text/css' href='/static/nprogress.css' />
+        </Head>
+
         <Menu secondary inverted fixed="top" color="teal">
           <Container fluid>
             <Menu.Item header><Link href="/"><a>KK-Pedia</a></Link></Menu.Item>

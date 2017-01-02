0957828f30dee9ed7427c68e54428924d65fde51
zarazi <zarazi@hotmail.com>
2017-01-02 16:23:31 +0700

    Pull story from service

--- pages/story.js
+++ pages/story.js
@@ -1,21 +1,40 @@
 import React from 'react'
+import 'isomorphic-fetch'
 import Head from 'next/head'
 import { Header } from 'semantic-ui-react'
 import Page from '../layouts/main'
 
+const URL_KKPEDIA_3 = 'https://script.google.com/macros/s/AKfycbwtHU7yTNFy7-Nya7cyIyLuVa8uQk_Pz-hwDJizd1E9nDuA-mw/exec';
+
 export default class extends React.Component {
-  static async getInitialProps({ url }) {}
+  static async getInitialProps({ query, req }) {
+    if (query && query.id) {
+      const res = await fetch(URL_KKPEDIA_3, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'action=getStoryById&id=' + query.id
+      })
+      const data = await res.json()
+      const isServer = !!req
+      return { data , isServer}
+    }
+  }
   render() {
-    const id = this.props.url.query.id
+    const which = this.props.isServer ? '[S]' : '[C]'
     return (
       <Page pathname={this.props.url.pathname} title={`Story`}>
-        <Header as="h1" dividing>Story { id && `(${id})` }</Header>
-        <p>
-          ตรงกันข้ามกับความเชื่อที่นิยมกัน Lorem Ipsum ไม่ได้เป็นเพียงแค่ชุดตัวอักษรที่สุ่มขึ้นมามั่วๆ แต่หากมีที่มาจากวรรณกรรมละตินคลาสสิกชิ้นหนึ่งในยุค 45 ปีก่อนคริสตศักราช ทำให้มันมีอายุถึงกว่า 2000 ปีเลยทีเดียว ริชาร์ด แมคคลินท็อค ศาสตราจารย์ชาวละติน จากวิทยาลัยแฮมพ์เด็น-ซิดนีย์ ในรัฐเวอร์จิเนียร์ นำคำภาษาละตินคำว่า consectetur ซึ่งหาคำแปลไม่ได้จาก Lorem Ipsum ตอนหนึ่งมาค้นเพิ่มเติม โดยตรวจเทียบกับแหล่งอ้างอิงต่างๆ ในวรรณกรรมคลาสสิก และค้นพบแหล่งข้อมูลที่ไร้ข้อกังขาว่า Lorem Ipsum นั้นมาจากตอนที่ 1.10.32 และ 1.10.33 ของเรื่อง "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) ของ ซิเซโร ที่แต่งไว้เมื่อ 45 ปีก่อนคริสตศักราช หนังสือเล่มนี้เป็นเรื่องราวที่ว่าด้วยทฤษฎีแห่งจริยศาสตร์ ซึ่งเป็นที่นิยมมากในยุคเรเนสซองส์ บรรทัดแรกของ Lorem Ipsum "Lorem ipsum dolor sit amet.." ก็มาจากบรรทัดหนึ่งในตอนที่ 1.10.32 นั่นเอง
-          <br/>
-          <br/>
-          ด้านล่างของหน้านี้คือท่อนมาตรฐานของ Lorem Ipsum ที่ใช้กันมาตั้งแต่คริสตศตวรรษที่ 16ที่ได้รับการสร้างขึ้นใหม่สำหรับผู้ที่สนใจ ประกอบไปด้วย ตอนที่ 1.10.32 และ 1.10.33 จากเรื่อง "de Finibus Bonorum et Malorum" โดยซิเซโร ก็ได้รับการผลิตขึ้นใหม่ด้วยเช่นกันในรูปแบบที่ตรงกับต้นฉบับ ตามมาด้วยเวอร์ชั่นภาษาอังกฤษจากการแปลของ เอช แร็คแคม เมื่อปี ค.ศ. 1914
-        </p>
+        {
+          this.props.data
+          ? !this.props.data.error
+            ? <div>
+                <Header as="h1" dividing>{ which } { this.props.data.name || 'No title'}</Header>
+                <p>{ this.props.data.text || 'No text' }</p>
+              </div>
+            : <pre>{this.props.data.error}</pre>
+          : <pre>No data found!</pre>
+        }
       </Page>
     )
   }
